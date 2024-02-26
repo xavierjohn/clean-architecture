549c48a5de1c154d5957623b99c70cacd807ff0d
Xavier <xavierjohn@hotmail.com>
2024-02-26 15:59:55 -0800

    Does not work.

--- src/CleanArchitecture.Application/Reminders/Commands/DeleteReminder/DeleteReminderCommandHandler.cs
+++ src/CleanArchitecture.Application/Reminders/Commands/DeleteReminder/DeleteReminderCommandHandler.cs
@@ -10,22 +10,17 @@ public class DeleteReminderCommandHandler(
     IRemindersRepository _remindersRepository,
     IUsersRepository _usersRepository) : IRequestHandler<DeleteReminderCommand, Result<FunctionalDdd.Unit>>
 {
-    public async Task<Result<FunctionalDdd.Unit>> Handle(DeleteReminderCommand request, CancellationToken cancellationToken)
-    {
-        var hReminderId = ReminderId.TryCreate(request.ReminderId);
-        if (hReminderId.IsFailure)
-        {
-            return hReminderId.Error;
-        }
-
-        return await UserId.TryCreate(request.UserId)
-                .BindAsync(userId => _usersRepository.GetByIdAsync(userId, cancellationToken).ToResultAsync(Error.NotFound("User not found")))
-                .ParallelAsync(_remindersRepository.GetByIdAsync(hReminderId.Value, cancellationToken).ToResultAsync(Error.NotFound("Reminder not found")))
-                .BindAsync((user, reminder) => user.DeleteReminder(reminder).Map(_ => user))
-                .BindAsync(user =>
-                {
-                    _usersRepository.UpdateAsync(user, cancellationToken);
-                    return Result.Success();
-                });
-    }
+    public async Task<Result<FunctionalDdd.Unit>> Handle(DeleteReminderCommand request, CancellationToken cancellationToken) =>
+        await ReminderId.TryCreate(request.ReminderId)
+            .Combine(UserId.TryCreate(request.UserId))
+            .BindAsync((redminderId, userId) =>
+                    _usersRepository.GetByIdAsync(userId, cancellationToken).ToResultAsync(Error.NotFound("User not found"))
+                    .ParallelAsync(_remindersRepository.GetByIdAsync(reminderId, cancellationToken).ToResultAsync(Error.NotFound("Reminder not found")))
+                    .BindAsync((user, reminder) =>
+                        user.DeleteReminder(reminder)
+                        .BindAsync(async _ =>
+                        {
+                            await _usersRepository.UpdateAsync(user, cancellationToken);
+                            return Result.Success();
+                        })));
 }
