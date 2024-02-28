d9595543c681943cf47f01490e2db7ba00c7c9df
Xavier <xavierjohn@hotmail.com>
2024-02-27 17:24:27 -0800

    DeleteReminderCommandHandler

--- src/CleanArchitecture.Application/Reminders/Commands/DeleteReminder/DeleteReminderCommandHandler.cs
+++ src/CleanArchitecture.Application/Reminders/Commands/DeleteReminder/DeleteReminderCommandHandler.cs
@@ -4,23 +4,21 @@ using CleanArchitecture.Domain.Users;
 
 using MediatR;
 
+using Unit = FunctionalDdd.Unit;
+
 namespace CleanArchitecture.Application.Reminders.Commands.DeleteReminder;
 
 public class DeleteReminderCommandHandler(
     IRemindersRepository _remindersRepository,
     IUsersRepository _usersRepository) : IRequestHandler<DeleteReminderCommand, Result<FunctionalDdd.Unit>>
 {
-    public async Task<Result<FunctionalDdd.Unit>> Handle(DeleteReminderCommand request, CancellationToken cancellationToken) =>
-        await ReminderId.TryCreate(request.ReminderId)
-            .Combine(UserId.TryCreate(request.UserId))
-            .BindAsync((redminderId, userId) =>
+    public async Task<Result<Unit>> Handle(DeleteReminderCommand request, CancellationToken cancellationToken) =>
+        await UserId.TryCreate(request.UserId)
+            .Combine(ReminderId.TryCreate(request.ReminderId))
+            .BindAsync((userId, reminderId) =>
                     _usersRepository.GetByIdAsync(userId, cancellationToken).ToResultAsync(Error.NotFound("User not found"))
                     .ParallelAsync(_remindersRepository.GetByIdAsync(reminderId, cancellationToken).ToResultAsync(Error.NotFound("Reminder not found")))
                     .BindAsync((user, reminder) =>
                         user.DeleteReminder(reminder)
-                        .BindAsync(async _ =>
-                        {
-                            await _usersRepository.UpdateAsync(user, cancellationToken);
-                            return Result.Success();
-                        })));
+                        .TapAsync(_ => _usersRepository.UpdateAsync(user, cancellationToken))));
 }
